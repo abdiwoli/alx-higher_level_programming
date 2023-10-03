89059794cc45816dbc26fc7da7503b5d5b00f935
abdiwoli <abdiwolih@gmail.com>
2023-10-04 01:36:34 +0300

    done

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def islower(s):
     if (s == '' or s is None):
+        print("Traceback (most recent call last):")
         exit()
     if 'a' <= s <= 'z':
         return True
