d2c3a0a3bc4d650ddae94ece5dbd88faf7176c63
abdiwoli <abdiwolih@gmail.com>
2023-10-03 18:22:18 +0300

    done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+init = 0;
 for i in range(0, 99):
     if (i < 10):
-        print(0, end="")
-    print(f"{i}, ", end="");
+        init = 0
+    else:
+        init = ""
+    print("{0}{1}, ".format(init, i), end="");
 print(99);
