3d6c7b0793b42d807f07ed78f1d703a13a62ccd6
abdiwoli <abdiwolih@gmail.com>
2023-10-03 23:57:14 +0300

    fic

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    flag = 1
+    if number < 0:
+        number = number * -1
+        flag = -1
     if number > 9:
         number = number % 10
-    return (number)
+    print(number, end="")
+    return (number);
