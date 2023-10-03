734c8c301a9d034f699bcfc99b62fa3855eae4da
abdiwoli <abdiwolih@gmail.com>
2023-10-04 01:09:40 +0300

    done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 def chec(v):
-    if (int(v) < 10):
+    if int(v) < 10:
         return False
-    if (int(v[1] + v[0]) > int(v)):
-        return True
-    return False
+    return sorted(v) == list(v)
 
-for v in range(1, 90):
-    if v < 10:
-       s = "0" + str(v)
-    else:
-       s = str(v)
-    if chec(s):
-        continue
-    print("{0}".format(s), end=", ")
-print(89)
+for i in range(10):
+    for j in range(i + 1, 10):
+        s = f"{i}{j}"
+        if chec(s) or j < 10:
+            print("{0}".format(s), end=", ")
+
+print()
