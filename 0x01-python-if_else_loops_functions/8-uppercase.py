6db23a2177e889edf6f4535e742a174856cb19fc
abdiwoli <abdiwolih@gmail.com>
2023-10-04 01:47:29 +0300

    upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+def islower(s):
+    if (s == '' or s is None):
+        print("Traceback (most recent call last):")
+        exit()
+    if 'a' <= s <= 'z':
+        return True
+    return False
+def uppercase(str):
+    for i in str:
+        if (islower(i)):
+            i = chr(ord(i) - ord('a') + ord('A'))
+        print("{0}".format(i), end="")
+print()
+
+        
