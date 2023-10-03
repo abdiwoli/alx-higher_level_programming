bbb168b86f310dc17acb6c68c5388ad72ca15308
abdiwoli <abdiwolih@gmail.com>
2023-10-03 23:58:23 +0300

    lastdigigt

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    flag = 1
     if number < 0:
         number = number * -1
-        flag = -1
     if number > 9:
         number = number % 10
     print(number, end="")
-    return (number);
+    return (number)
