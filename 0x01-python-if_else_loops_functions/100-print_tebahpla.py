569e998410a5bb5c240e05f40bb56e7d578c252e
abdiwoli <abdiwolih@gmail.com>
2023-10-04 02:11:10 +0300

    advance

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 n = 0;
-for i in "zyxwvtsrqponmlkjihgfedcba":
+for i in "zyxwvutsrqponmlkjihgfedcba":
     if (n % 2 == 1):
         i = chr(ord(i) - ord('a') + ord('A'))
     print("{0}{1}".format(i, "\n" if i == "A" else ""), end="")
