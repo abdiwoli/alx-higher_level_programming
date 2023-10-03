128e804bf5868ba77e17920341b6b38fa6534f33
abdiwoli <abdiwolih@gmail.com>
2023-10-04 01:48:29 +0300

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 #!/usr/bin/python3
 def islower(s):
-    if (s == '' or s is None):
-        print("Traceback (most recent call last):")
         exit()
     if 'a' <= s <= 'z':
         return True
