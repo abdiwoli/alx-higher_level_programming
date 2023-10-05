950c4cd95d44b33232bf469f0f7210c93fec12f4
abdiwoli <abdiwolih@gmail.com>
2023-10-05 17:29:31 +0300

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import sys
+if __name__ != "__main__":
+    exit()
+
 n = len(sys.argv) -1
 if n == 1:
     m = f"1 argument:"
@@ -11,5 +14,5 @@ print(m)
 i = 1
 for j, i in enumerate(range(1, n)):
     print("{}: {}".format(j, sys.argv[i]))
-f n > 0:
-    print("{}: {}".format(n, sys.argv[n])
+if n > 0:
+    print("{}: {}".format(n, sys.argv[n]))
