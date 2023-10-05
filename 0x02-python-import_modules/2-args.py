d542ed5a838daba290d4f2bf6939d5d6891c8834
abdiwoli <abdiwolih@gmail.com>
2023-10-05 17:23:33 +0300

    done2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+n = len(sys.argv) -1
+if n == 1:
+    m = f"1 argument:"
+elif n == 0:
+    m = "0 arguments."
+else:
+    m = "{} arguments:".format(n)
+print(m)
+i = 1
+for j, i in enumerate(range(1, n)):
+    print("{}: {}".format(j, sys.argv[i]))
+f n > 0:
+    print("{}: {}".format(n, sys.argv[n])
