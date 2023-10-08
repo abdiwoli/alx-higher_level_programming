7c4dc7c112ef2edfd353200d66809d6fa7530ab1
abdiwoli <abdiwolih@gmail.com>
2023-10-08 21:46:52 +0300

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-	for i in matrix:
-		for j in i:
-			print("{}".format(j), end=" ")
-		print()
-
+    for i in matrix:
+        n = len(i) - 1
+        for v, j in enumerate(i):
+            t = " " if v < n else ""
+            print("{}".format(j), end=t)
+        print()
