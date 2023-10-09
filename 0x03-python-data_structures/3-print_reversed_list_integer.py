4053da807cdcb15bcd91c769ea9eaead5a31b243
abdiwoli <abdiwolih@gmail.com>
2023-10-09 18:57:29 +0300

    fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-	n = len(my_list)
-	for el in range(n, 0,-1):
-		print("{}".format(el))
-
+    n = len(my_list)
+    for el in range(n, 0, -1):
+        print("{}".format(el))
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-	st = ""
-	for i in my_string:
-		if i == "c" or i == "C":
-			continue
-		st += i
-	return st
-
+    st = ""
+    for i in my_string:
+        if i == "c" or i == "C":
+            continue
+    st += i
+    return st
