8190b8630c31c8013294436cfc6824f3e14b4dc8
abdiwoli <abdiwolih@gmail.com>
2023-10-11 20:14:39 +0300

    fix10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def best_score(a = None):
+def best_score(a):
     if a is None:
         return None
     arr = [i for i in a.keys()]
     big = arr[0]
 
-    for i in arr[1: ]:
+    for i in arr[1:]:
         if a[i] > a[big]:
             big = i
     return big
