c956c526a358967841609016d87696dbd53bb362
abdiwoli <abdiwolih@gmail.com>
2023-10-12 02:04:34 +0300

    done

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    d = {"I": 1, "IV": 4, "IX": 9, "V": 5, "X": 10, "L": 50, "C": 100}
-    d.update({"D": 500, "M": 1000})
-    d.update({"i": 1, "iv": 4, "v": 5, "ix": 9, "x": 10})
-    d.update({"l": 50, "c": 100, "d": 500, "m": 1000})
-    values = [i for i in d.keys()]
-    if (roman_string in values):
-        return d[roman_string]
-    arr = []
-    for i in roman_string:
-        arr.append(d[i])
-    return sum(arr)
+
+    d = {
+        "I": 1, "IV": 4, "IX": 9, "V": 5, "X": 10,
+        "L": 50, "C": 100, "D": 500, "M": 1000,
+        "i": 1, "iv": 4, "v": 5, "ix": 9, "x": 10,
+        "l": 50, "c": 100, "d": 500, "m": 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = d.get(char, 0)
+
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+
+        prev_value = value
+
+    return total
