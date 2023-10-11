990824331589b3eb21f3bf53d257c6f7a7bd79b1
abdiwoli <abdiwolih@gmail.com>
2023-10-12 00:21:06 +0300

    done13

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(d, value):
+    if d is None:
+        return None
+    k = [i for i, v in d.items() if v== value]
+    for i in k:
+        del d[i]
+    return d
