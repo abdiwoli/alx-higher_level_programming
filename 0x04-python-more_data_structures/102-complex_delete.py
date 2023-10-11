932e653f3c660b31c5769e403df844e1dd54b041
abdiwoli <abdiwolih@gmail.com>
2023-10-12 00:23:02 +0300

    fix15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(d, value):
     if d is None:
         return None
-    k = [i for i, v in d.items() if v== value]
+    k = [i for i, v in d.items() if v == value]
     for i in k:
         del d[i]
     return d
