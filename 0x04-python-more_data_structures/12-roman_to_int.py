c0edd54d90b9eecbaeaa159ce444af0f80f279d8
abdiwoli <abdiwolih@gmail.com>
2023-10-12 00:46:24 +0300

    fix12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    d = {"I": 1, "IV": 4, "IX": 9, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    d.update({"i": 1,"iv": 4, "v": 5, "ix": 9, "x": 10, "l": 50, "c": 100, "d": 500, "m": 1000})
+    d = {"I": 1, "IV": 4, "IX": 9, "V": 5, "X": 10, "L": 50, "C": 100}
+    d.update({"D": 500, "M": 1000})
+    d.update({"i": 1, "iv": 4, "v": 5, "ix": 9, "x": 10})
+    d.update({"l": 50, "c": 100, "d": 500, "m": 1000})
     values = [i for i in d.keys()]
     if (roman_string in values):
         return d[roman_string]
