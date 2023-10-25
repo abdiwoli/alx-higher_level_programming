76b6500c2a6daf3e28c49443d7ba10013ffeb60f
abdiwoli <abdiwolih@gmail.com>
2023-10-25 05:29:07 +0300

    done103

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+class MagicClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.sum = 0
+
+    def calculate(self):
+        for n in range(1, 3):
+            try:
+                if n > self.a:
+                    raise Exception("Too far")
+                self.sum += (self.a ** self.b) / n
+            except Exception:
+                self.sum = self.b + self.a
+                break
+
+        return self.sum
