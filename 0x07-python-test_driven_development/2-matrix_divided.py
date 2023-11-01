e2929e79ed22f8ff7a65d9cb4612e6cf5d97433b
abdiwoli <abdiwolih@gmail.com>
2023-11-01 17:07:35 +0300

    done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
+""" module doc"""
+
+
 def matrix_divided(matrix, div):
+    """
+    function devides matrix
+
+    Args:
+        matrix: lis of list
+        div: number
+
+    Return:
+        new matrix
+    """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    if not all(isinstance(row, list) and all(isinstance(item, (int, float)) for item in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    m = "matrix must be a matrix (list of lists) of integers/floats"
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(m)
+        for item in row:
+            if not isinstance(item, (int, float)):
+                raise TypeError(m)
     
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
