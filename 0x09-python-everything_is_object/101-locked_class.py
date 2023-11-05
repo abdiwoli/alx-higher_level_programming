647e8b2f11762ba333cf9fca62f7bd15175c44e5
abdiwoli <abdiwolih@gmail.com>
2023-11-06 02:01:44 +0300

    done

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,6 +2,8 @@
 """class lock"""
 
 class LockedClass:
+    """ class lock ""
+
     def __setattr__(self, name, value):
         if hasattr(self, name) or name == "first_name":
             super().__setattr__(name, value)
