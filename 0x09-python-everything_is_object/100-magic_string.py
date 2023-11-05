2a699de4d5a5c064f31166bb282105c746156197
abdiwoli <abdiwolih@gmail.com>
2023-11-06 01:28:47 +0300

    done

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,6 +2,3 @@
 def magic_string():
     magic_string.c = 'BestSchool' if not hasattr(magic_string, 'c') else magic_string.c + ', BestSchool'
     return magic_string.c
-
-for i in range(10):
-    print(magic_string())
