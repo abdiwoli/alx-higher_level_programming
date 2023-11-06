aba3639303dbbc35ffdf8100451e2366a0dc46cd
abdiwoli <abdiwolih@gmail.com>
2023-11-06 18:32:11 +0300

    done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+""" import module"""
+
+
 def lookup(obj):
+    """ function lookup
+
+    Args:
+        obj: object
+
+    Return:
+        return: list
+     """
     return [i for i in obj.__dict__]
