c397034eb29f201381ff81ad3d2c03db7b952d2f
abdiwoli <abdiwolih@gmail.com>
2023-11-07 01:09:34 +0300

    done

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module containing is_same_class method"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare the object's instance 
+
+    Returns:
+        bool: True or False
+
+    Example:
+        >>> a = 1
+        >>> is_same_class(a, int)
+        True
+
+    """
+    return isinstance(obj, a_class)
