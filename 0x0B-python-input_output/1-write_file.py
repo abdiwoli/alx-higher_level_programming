2362065c724029bc3dbb0b7ca9cff364aeb2aadf
abdiwoli <abdiwolih@gmail.com>
2023-11-07 17:53:05 +0300

    done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """function writes to a file and returns the character count"""
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
