7253a13661cd9e43d4302bccfe8e0c143221f07c
abdiwoli <abdiwolih@gmail.com>
2023-11-07 17:51:28 +0300

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """function reads file"""
     with open(filename, "r", encoding="utf-8") as file:
-        content = file.read()
-        print(content)
+        for content in file:
+            print(content)
