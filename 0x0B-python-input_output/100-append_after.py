b1150e56dd2b311b7bec294557207f7c2c5b42ff
abdiwoli <abdiwolih@gmail.com>
2023-11-07 23:31:50 +0300

    done

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,11 +6,21 @@ def append_after(filename="", search_string="", new_string=""):
     """append function"""
     with open(filename, "r") as file:
         lines = []
+        append_next_line = False
+
         for line in file:
+            if append_next_line:
+                # Append the new string after the next comma
+                index = line.find(',')
+                if index != -1:
+                    line = line[:index + 1] + new_string + line[index + 1:]
+
+                append_next_line = False
+
+            lines.append(line)
+
             if search_string in line:
-                lines.append(new_string)
-                lines.append(line)
-            else:
-                lines.append(line)
+                append_next_line = True
+
     with open(filename, "w") as file:
         file.writelines(lines)
