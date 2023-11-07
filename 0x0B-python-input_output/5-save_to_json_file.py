846ad579089ccf435b69713739ec459297b7f1f6
abdiwoli <abdiwolih@gmail.com>
2023-11-07 20:12:05 +0300

    done

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,7 +3,8 @@
 import json
 
 
+
 def save_to_json_file(my_obj, filename):
     """function writes tow file"""
-    with open(filename, "w", endcode=encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         json.dump(my_obj, file, ensure_ascii=False)
