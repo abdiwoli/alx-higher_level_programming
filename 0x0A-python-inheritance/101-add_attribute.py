1d5f2a5834ad79b159c7632f244dedbd852f3fcb
abdiwoli <abdiwolih@gmail.com>
2023-11-09 00:24:04 +0300

    fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,7 @@
 
 def add_attribute(obj, attr_name, attr_value):
     """add_attribute function"""
-    if isinstance(obj, (str, int, float, list, dict)):
+    al = (str, int, float, list, dict)
+    if isinstance(obj, al) or attr_name not in obj.__slots__:
         raise TypeError("can't add new attribute")
     setattr(obj, attr_name, attr_value)
