f262ddd1f76221df0aaf646182f83990b12a7801
abdiwoli <abdiwolih@gmail.com>
2023-11-09 00:36:21 +0300

    fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,8 @@
 
 def add_attribute(obj, attr_name, attr_value):
     """add_attribute function"""
-    al = (str, int, float, list, dict)
-    if isinstance(obj, al) or attr_name not in obj.__slots__:
+    slots = getattr(obj.__class__, '__slots__', None)
+    al = (int, str, dict, list, tuple)
+    if isinstance(obj, al) or slots is not None and attr_name not in slots:
         raise TypeError("can't add new attribute")
     setattr(obj, attr_name, attr_value)
--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -2,11 +2,8 @@
 add_attribute = __import__('101-add_attribute').add_attribute
 
 class MyClass():
-     __slots__ = ["first_name"]
+    pass
 
-try:
-    a = MyClass()
-    add_attribute(a, "hbtn", "Holberton")
-    print(a.name)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+mc = 39
+add_attribute(mc, "name", "John")
+print(mc.name)
