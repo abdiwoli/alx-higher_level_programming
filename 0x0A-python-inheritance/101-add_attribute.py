3192a3dbabc9a7acd0a86899c13853f5e8879246
abdiwoli <abdiwolih@gmail.com>
2023-11-08 20:04:08 +0300

    done

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,6 @@
 
 def add_attribute(obj, attr_name, attr_value):
     """add_attribute function"""
-    if not type(obj) == str:
+    if not isinstance(obj, type(attr_value)):
         raise TypeError("can't add new attribute")
     setattr(obj, attr_name, attr_value)
--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -2,11 +2,15 @@
 add_attribute = __import__('101-add_attribute').add_attribute
 
 class MyClass():
-     __slots__ = ["first_name"]
+    pass
+
+mc = MyClass()
+add_attribute(mc, "name", "John")
+print(mc.name)
 
 try:
-    a = MyClass()
-    add_attribute(a, "hbtn", "Holberton")
+    a = "My String"
+    add_attribute(a, "name", "Bob")
     print(a.name)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
