b3b8d2375142e36c9303fad1ffcc68eb0dc05ad2
abdiwoli <abdiwolih@gmail.com>
2023-11-15 12:29:38 +0300

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -59,6 +59,8 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        if json_string is None:
+            return []
         return json.loads(json_string)
 
     @classmethod
