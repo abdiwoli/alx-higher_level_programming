bdaab42105352b9415f2f982731d79b74a6de57f
abdiwoli <abdiwolih@gmail.com>
2023-12-13 01:19:05 +0300

    showname

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,15 +1,4 @@
--- Change the database character set and collation
-ALTER DATABASE hbtn_0c_0
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
-
--- Change the table character set and collation
-ALTER TABLE hbtn_0c_0.first_table
-  CONVERT TO CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
-
--- Change the field character set and collation
-ALTER TABLE hbtn_0c_0.first_table
-  MODIFY COLUMN name VARCHAR(256)
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
+-- Converts the entire database hbtn_0c_0 to UTF8.
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
