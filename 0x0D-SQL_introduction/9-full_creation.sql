ab22b3c23c8029d069722e1baf414d35c2576a39
abdiwoli <abdiwolih@gmail.com>
2023-12-12 23:55:35 +0300

    insert

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS second_table (
 
 -- insert data
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (2, 'aLEX', 14);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 14);
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
