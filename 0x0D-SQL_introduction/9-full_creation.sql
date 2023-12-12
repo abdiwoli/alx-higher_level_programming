412d12ad52281133866e56671134a02dcc794f60
abdiwoli <abdiwolih@gmail.com>
2023-12-12 23:51:13 +0300

    insert

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- create table
+CREATE TABLE IF NOT EXISTS second_table
+(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO first_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO first_table (id, name, score) VALUES (2, 'aLEX', 14);
+INSERT INTO first_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO first_table (id, name, score) VALUES (4, 'George', 8);
