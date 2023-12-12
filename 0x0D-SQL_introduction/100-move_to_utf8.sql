9551454e4dcdf876d6442f403c05d55a803ef373
abdiwoli <abdiwolih@gmail.com>
2023-12-13 01:17:41 +0300

    showname

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,15 @@
+-- Change the database character set and collation
+ALTER DATABASE hbtn_0c_0
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+-- Change the table character set and collation
+ALTER TABLE hbtn_0c_0.first_table
+  CONVERT TO CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+-- Change the field character set and collation
+ALTER TABLE hbtn_0c_0.first_table
+  MODIFY COLUMN name VARCHAR(256)
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
