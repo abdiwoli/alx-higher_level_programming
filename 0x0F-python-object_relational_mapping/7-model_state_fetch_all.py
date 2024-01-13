d0b3db8d2633b7045e669841996256f6f742870a
abdiwoli <abdiwolih@gmail.com>
2024-01-12 17:36:04 -0800

    sqlal

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine, text
+
+
+if __name__ == "__main__":
+    us = sys.argv[1]
+    ps = sys.argv[2]
+    db = sys.argv[3]
+    p = 'mysql+mysqldb://{}:{}@localhost/{}'.format(us, ps, db)
+    engine = create_engine(p, pool_pre_ping=True)
+
+    q = text("SELECT * FROM states ORDER BY id;")
+
+    result = engine.execute(q)
+
+    for i in result:
+        print(i)
