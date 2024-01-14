5fbd3547c2f986557a23ed191558cbc9b649385e
abdiwoli <abdiwolih@gmail.com>
2024-01-14 01:06:29 -0800

    done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__" and len(sys.argv) == 4:
                           port=3306, user=user,
                           passwd=passw, db=db)
     cur = con.cursor()
-    q = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;"
+    q = "SELECT * FROM states WHERE name LIKE UPPER('n%') ORDER BY id;"
     cur.execute(q)
     row = cur.fetchone()
     while row:
