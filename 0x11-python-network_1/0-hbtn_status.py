96b5481f927a7129ff010592371c78a0fa11385a
abdiwoli <abdiwolih@gmail.com>
2024-01-26 12:44:15 -0800

    try

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ from urllib import request
 if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as fetch:
         contb = fetch.read()
-        conts = contb.decode()
+        conts = contb.decode("utf-8")
         print("Body response:")
         print(f"    - type: {type(contb)}")
         print(f"    - content: {contb}")
