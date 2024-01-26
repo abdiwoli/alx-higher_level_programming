9cca6d26ecb89d6938df9b4f79219fc8dd606aab
abdiwoli <abdiwolih@gmail.com>
2024-01-26 15:11:40 -0800

    done

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" post request with paramater """
+from sys import argv
+import requests
+
+
+def send_post(q):
+    data = {'q': q}
+    res = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        jsn = res.json()
+        if jsn == "{}":
+            print("No result")
+        else:
+            print("[{}] {}".format(jsn.get("id"), jsn.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    send_post(q)
