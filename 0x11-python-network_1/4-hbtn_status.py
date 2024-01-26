6f2b71e43ce1811aef319681cb6ad54cb28e200b
abdiwoli <abdiwolih@gmail.com>
2024-01-26 14:15:30 -0800

    done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     fetch = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print(f"\t- type: {type(fetch)}")
+    print(f"\t- type: {type(fetch.text)}")
     print(f"\t- content: {fetch.text}")
