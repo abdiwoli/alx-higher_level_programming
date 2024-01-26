5ed3960c38f84bab1293ad088fee2395c14b8e6b
abdiwoli <abdiwolih@gmail.com>
2024-01-26 13:28:09 -0800

    done

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     try:
         with request.urlopen(argv[1]) as fetch:
             contb = fetch.read()
-            conts = contb.decode("utf-8")
-            print(counts)
+            conts = contb.decode("ascii")
+            print(conts)
     except HTTPError as e:
         print("Error code: {}".format(e.code))
