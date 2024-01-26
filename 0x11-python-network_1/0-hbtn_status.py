847ef9a5f20d99661a69b461df3902a6532b2dc8
abdiwoli <abdiwolih@gmail.com>
2024-01-26 12:41:00 -0800

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """ what is my ip"""
 from urllib import request
 
-
-with request.urlopen("https://alx-intranet.hbtn.io/status") as fetch:
-    contb = fetch.read()
-    conts = contb.decode()
-    print("Body response:")
-    print(f"    - type: {type(contb)}")
-    print(f"    - content: {contb}")
-    print(f"    - utf8 content: {conts}")
+if __name__ == "__main__":
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as fetch:
+        contb = fetch.read()
+        conts = contb.decode()
+        print("Body response:")
+        print(f"    - type: {type(contb)}")
+        print(f"    - content: {contb}")
+        print(f"    - utf8 content: {conts}")
