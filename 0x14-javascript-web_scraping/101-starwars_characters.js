d2da8549aaf488b78f9d3d2a652d9f77b15296ee
abdiwoli <abdiwolih@gmail.com>
2024-02-20 20:07:55 +0300

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
-const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+require('request')(url, function (error, response, body) {
   if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+    const result = JSON.parse(body).characters;
+    slowPrint(result, 0);
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
+function slowPrint (characters, index) {
+  require('request')(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+        slowPrint(characters, index + 1);
       }
     }
   });
