ca88ba94d6bbe775b186ea1dba2021454ad5490a
abdiwoli <abdiwolih@gmail.com>
2024-02-20 16:30:37 +0300

    done

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 require('https').get(process.argv[2], (response) => {
-  console.log('code:', response.statusCode);
+  console.log(`code: ${response.statusCode}`);
 }).on('error', (error) => {
   console.error(`Error making GET request: ${error.message}`);
 });
